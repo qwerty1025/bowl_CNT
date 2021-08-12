9e3c6bf59139b9d0e38611c5cc00df8eb8a009c1
qwerty1025 <s9921316@gmail.com>
2021-08-12 10:53:46 +0800

    2_uplaod

---
+++ src/Firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBcH9bj8Bm5vSZPbaBnBy-FS9IaGA_iI1M",
+    authDomain: "fire-base-demo-0811.firebaseapp.com",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET"
+  };
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyBcH9bj8Bm5vSZPbaBnBy-FS9IaGA_iI1M",
+    authDomain: "fire-base-demo-0811.firebaseapp.com",
+    projectId: "fire-base-demo-0811",
+    storageBucket: "fire-base-demo-0811.appspot.com",
+    messagingSenderId: "236106228674",
+    appId: "1:236106228674:web:0682d496d91782c16fac5b",
+    measurementId: "G-K3QBQV5GH0"
+  };
